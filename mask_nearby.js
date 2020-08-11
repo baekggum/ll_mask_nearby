e8e4abbe39efda9d888ed52af75893ca72266895
BaekGGum <c3man4@naver.com>
2020-08-11 12:08:06 +0900

    test

---
+++ mask_nearby.js
@@ -0,0 +1,51 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+var ps = new kakao.maps.services.Places();
+
+// 마스크 데이터 api 주소
+let base_mask_url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json";
+// 버튼 누르거나 Enter 눌렀을 때 검색이 되도록 만들기
+let search_btn = document.querySelector(".search-btn");
+let search_bar = document.querySelector("#search-bar");
+
+search_btn.addEventListener("click", ()=>{
+    let keyword = search_bar.value;
+    if(keyword){
+        console.log(keyword+" 검색하셨습니다.");
+        keywordSearch(keyword);
+    }else{
+        alert("검색어를 입력해주세요.");
+    }
+})
+
+search_bar.addEventListener("keyup", () => {
+    // keycode 13 = Enter key
+    if(event.keyCode === 13){
+        search_btn.click();
+    }
+})
+
+function keywordSearch(keyword){
+    ps.keywordSearch(keyword,placesSearchCallBack);
+}
+
+function placesSearchCallBack (data, status, pagination) {
+    if(status === kakao.maps.services.Status.OK){
+        let center = new kakao.maps.LatLng(data[0].y,data[0].x);
+        map.setCenter(center);
+        getMaskData(data[0].y,data[0].x);
+    } 
+}
+
+async function getMaskData(Lat,Lng){
+    let request_url = `${base_mask_url}lat=${Lat}&lng=${Lng}`;
+    let response = await fetch(request_url);
+    let result = await response.json();
+    console.log(result.stores);
+    return result.LatLng
+}
